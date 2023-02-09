64d63e999476fc9033e0673089b3ad91c55f1179
Anastasiia4435 <107216310+Anastasiia4435@users.noreply.github.com>
2023-02-09 19:53:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let a = 25;
+let b = 35;
+
+let d = (a+b) *2;
+let c = a*b;
+
+alert(d);
+
+alert(c);
+
+let j = 10;
+
+let o = j+10;
+let k = j*20;
+let y = j-30;
+let m = j/5;
+
+alert(o);
+alert(k);
+alert(y);
+alert(m);
